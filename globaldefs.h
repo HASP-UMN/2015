68c7426a7c70e20057c1e339cb801193a69c563f
Josiah DeLange <delan231@umn.edu>
2015-06-17 14:50:57 -0500

    Further edit of globaldefs file

--- globaldefs.h
+++ globaldefs.h
@@ -4,12 +4,12 @@
 #define NSECS_PER_SEC           1000000000                      ///< [nsec/sec] nanoseconds per second */
 #define D2R                     0.017453292519943               ///< [rad] degrees to radians */
 #define R2D                     57.295779513082323              ///< [deg] radians to degrees */
-#define PSI_TO_KPA              6.89475729                      ///< [KPa] PSI to KPa */
 #define g                       9.814                           ///< [m/sec^2] gravity */
 #define g2                      19.62                           ///< [m/sec^2] 2*g */
 #define PI                      3.14159265358979                ///< pi */
 #define PI2                     6.28318530717958                ///< pi*2 */
 #define half_pi                 1.57079632679490                ///< pi/2 */
+
 #define BUFMAX 500
 
 #ifndef TRUE
@@ -22,77 +22,95 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-#include "spi.h"
 
-typedef unsigned char   byte;           ///< typedef of byte
-typedef unsigned short  word;           ///< typedef of word
 
-// buffer for storing photon data from FIFO in irq handler
-extern const unsigned long LENGTH = 500;
-extern unsigned char	PHOTON_DATA_BUFFER[LENGTH];
-extern int BYTES_PER_PHOTON = 10;
-extern int PHOTONS_AQUIRED = 0;
+// DATA TYPES
+typedef unsigned char   byte;           //  typedef of byte
+typedef unsigned short  word;           //  typedef of word
 
-//PORT ADDRESS ON THE ISA BUS
-const unsigned short INPUT_PORT = 0x800;
+
+// PHOTON DATA BUFFER (FROM FIFO) FOR IRQ HANDLER
+extern const unsigned long  LENGTH = 500;
+extern unsigned char        PHOTON_DATA_BUFFER[LENGTH];
+extern int                  BYTES_PER_PHOTON = 10;
+extern int                  PHOTONS_AQUIRED = 0;
+
+
+// ISA BUS INPUT PORT
+const unsigned short INPUT_PORT = 0x800; // base address
 int SYNC_BYTE = 77; //arbitrarily chosen for now
 int IRQ = 6;
 
 
-struct imu
-{
-    //unsigned long time;
-    long stime;
-    long mtime;
-    uint16_t supply_raw;
-    int gyroX_raw;
-    int gyroY_raw;
-    int gyroZ_raw;
-    int accelX_raw;
-    int accelY_raw;
-    int accelZ_raw;
-    int magX_raw;
-    int magY_raw;
-    int magZ_raw;
-    int temp_raw;
-    uint16_t ADC_raw;
-    int *response;
-    int spi_handle;
-    int badDataCounter;
-    unsigned long rstTime;
-};
-
-struct photons
-{
-    //unsigned long time;
-    int channel01;
-    int channel02;
-    int channel03;
-    int channel04;
-    //int spi_handle;
-};
-
+// STRUCTURE FOR OEMSTAR DATA (BESTXYZB)
 struct gps
 {
+    // RECEIVER CHARACTERISTICS
+    int port;                       // GPS receiver serial communication port
+    char responseBuffer[BUFFMAX];	// Character buffer for response data
+    int readCalls;                  // Number of times read_gps has been called
+    int badDataCounter;             // Number of bad read from read_gps (rests after 5)
+    int posValFlag;                 // Flag signifying valid XYZ position from receiver
+    unsigned long lastPosVal;       // Last position valid
     
-    uint8_t timeStatus;			// Time status
-    uint16_t weekRef;			// GPS week reference number
-    long time;				    // GPS timestamp [s]
+    // GPS TIME (TELEMETRY PACKET BYTES 1-2)
+    uint8_t timeStatus;             // Time status
+    uint16_t weekRef;               // GPS week reference number
+    long time;                      // GPS timestamp [s]
+ 
+    // GPS ECEF X (TELEMETRY PACKET BYTES 3-9)
+    double Xe;                      // X position (ECEF) [m]
+    float  P_Xe;                    // X std dev [m]
+    
+    // GPS ECEF Y (TELEMETRY PACKET BYTES 10-18)
+    double Ye;                      // Y position (ECEF) [m]
+    float  P_Ye;                    // Y std dev [m]
+    
+    // GPS ECEF Z (TELEMETRY PACKET BYTES 19-27)
+    double Ze;                      // Z position (ECEF) [m]
+    float  P_Ze;                    // Z std dev [m]
+};
 
-    double Xe;                  // X position (ECEF) [m]
-    double Ye;                  // Y position (ECEF) [m]
-    double Ze;                  // Z position (ECEF) [m]
-    float  Px;                  // X std dev [m]
-    float  Py;                  // Y std dev [m]
-    float  Pz;                  // Z std dev [m]
+
+// STRUCTURE FOR PHOTON DATA (FROM CMOS AYNCHRONOUS FIFO, VIA THE ISA BUS)
+struct photons
+{
+    // CHANNEL 1 PHOTON EVENTS (TELEMETRY PACKET BYTES 37-45)
+    int counts_ch01;    // number of gamma photons stopped by detector 1
     
-    int port;                   // GPS receiver serial communication port
-    char responseBuffer[144];	// Character buffer for response data
-    int readCalls;              // Number of times read_gps has been called
-    int badDataCounter;         // Number of bad read from read_gps (rests after 5)
-    int posValFlag;             // Flag signifying valid XYZ position from receiver
-    unsigned long lastPosVal;   // Last position valid
+    // CHANNEL 2 PHOTON EVENTS (TELEMETRY PACKET BYTES 46-54)
+    int counts_ch02;    // number of gamma photons stopped by detector 2
     
+    // CHANNEL 3 PHOTON EVENTS (TELEMETRY PACKET BYTES 55-63)
+    int counts_ch03;    // number of gamma photons stopped by detector 3
+    
+    // CHANNEL 4 PHOTON EVENTS (TELEMETRY PACKET BYTES 64-72)
+    int counts_ch04;    // number of gamma photons stopped by detector 4
+};
+
+
+// STRUCTURE FOR VN100 DATA (REGISTER ID: 54)
+struct imu
+{
+    // SERIAL I/O RESPONSE
+    int port;
+    char dataBuffer[131];
+    
+    // AMBIENT PAYLOAD TEMPERATURE (TELEMETRY PACKET BYTES 73-78)
+    float Temp;     // [C]
+    
+    // OTHER CALIBRATED SENSOR MEASUREMENTS
+    float MagX;     // [gauss]
+    float MagY;     // [gauss]
+    float MagZ;     // [gauss]
+    float AccelX;   // [m/s^2]
+    float AccelY;   // [m/s^2]
+    float AccelZ;   // [m/s^2]
+    float GyroX;    // [rad/s]
+    float GyroY;    // [rad/s]
+    float GyroZ;    // [rad/s]
+    // ambient temperature measurement comes here in the response packet
+    float Pressure; // [kPa]
 };
 
 #endif
