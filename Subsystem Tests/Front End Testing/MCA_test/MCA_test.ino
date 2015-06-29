481e2fd411e5f366ba4da4c4d829c1ae6e4cf97e
Josiah DeLange <delan231@umn.edu>
2015-06-29 16:24:50 -0500

    Need to define ATmega2560 I/O pins

--- Subsystem Tests/Front End Testing/MCA_test/MCA_test.ino
+++ Subsystem Tests/Front End Testing/MCA_test/MCA_test.ino
@@ -20,6 +20,23 @@
 #define IRQ_CH3 4    // Arduino Mega 2560 interrupt 4 (pin 19)
 #define IRQ_CH4 5    // Arduino Mega 2560 interrupt 5 (pin 18)
 
+/* DATA OUTPUT (NEED TO DETERMINE CORRECT PINS AND UPDATE)
+#define PF_0 A0
+#define PF_1 A1
+#define PF_2 A2
+#define PF_3 A3
+#define PF_4 A4
+#define PF_5 A5
+#define PF_6 A6
+#define PF_7 A7
+*/
+
+/* FIFO CONTROL (NEED TO DETERMINE CORRECT PINS AND UPDATE)
+#define FIFO_RST A8
+#define FIFO_WR A9
+#define FIFO_FF A10
+*/
+
 // Global variables
 const int ADCchipSelect = 53;        // Chip select for A/D converter
 volatile bool newEventCH1 = false;   // New event flag for MCA channel 1
@@ -35,30 +52,30 @@ uint16_t peakCH4 = 0;                // Peak value for MCA channel 4
 
 // Interrupt handlers
 void eventISR_CH1() {
-  
-  if(!newEventCH1 && !newEventCH2 && !newEventCH3 && !newEventCH4)
-    newEventCH1 = true;  
+
+  if (!newEventCH1 && !newEventCH2 && !newEventCH3 && !newEventCH4)
+    newEventCH1 = true;
   // Ignore new events if another event (on any channel) is currently being processed
 }
 
 void eventISR_CH2() {
-  
-  if(!newEventCH1 && !newEventCH2 && !newEventCH3 && !newEventCH4)
-    newEventCH2 = true;  
+
+  if (!newEventCH1 && !newEventCH2 && !newEventCH3 && !newEventCH4)
+    newEventCH2 = true;
   // Ignore new events if another event (on any channel) is currently being processed
 }
 
 void eventISR_CH3() {
-  
-  if(!newEventCH1 && !newEventCH2 && !newEventCH3 && !newEventCH4)
-    newEventCH3 = true;  
+
+  if (!newEventCH1 && !newEventCH2 && !newEventCH3 && !newEventCH4)
+    newEventCH3 = true;
   // Ignore new events if another event (on any channel) is currently being processed
 }
 
 void eventISR_CH4() {
-  
-  if(!newEventCH1 && !newEventCH2 && !newEventCH3 && !newEventCH4)
-    newEventCH4 = true;  
+
+  if (!newEventCH1 && !newEventCH2 && !newEventCH3 && !newEventCH4)
+    newEventCH4 = true;
   // Ignore new events if another event (on any channel) is currently being processed
 }
 
@@ -70,24 +87,38 @@ void setup() {
   pinMode(RESET_CH2, OUTPUT);
   pinMode(RESET_CH3, OUTPUT);
   pinMode(RESET_CH4, OUTPUT);
+  pinMode(PF_0, OUTPUT);
+  pinMode(PF_1, OUTPUT);
+  pinMode(PF_2, OUTPUT);
+  pinMode(PF_3, OUTPUT);
+  pinMode(PF_4, OUTPUT);
+  pinMode(PF_5, OUTPUT);
+  pinMode(PF_6, OUTPUT);
+  pinMode(PF_7, OUTPUT);
+  
+  pinMode(FIFO_SRST OUTPUT);
+  pinMode(FIFO_WR, OUTPUT);
+  pinMode(FIFO_FF, OUTPUT);
+  
+  //digitalWrite(A0, HIGH);...?
   
   digitalWrite(ADCchipSelect, HIGH);
   digitalWrite(RESET_CH1, LOW);
   digitalWrite(RESET_CH2, LOW);
   digitalWrite(RESET_CH3, LOW);
   digitalWrite(RESET_CH4, LOW);
-  
+
   // Open serial port
   Serial.begin(115200);
-  
+
   // Initialize and configure SPI bus for A/D communications
   SPI.begin();
   SPI.setClockDivider(SPI_CLOCK_DIV2);   // 8 MHz SPI clock
   SPI.setBitOrder(MSBFIRST);             // Most-significant bit first
   SPI.setDataMode(SPI_MODE0);            // Clock polarity = 0, clock phase = 0
-  
+
   delay(100);
-  
+
   // Write A/D configuration register to enable internal Vref and temperature sensor
   digitalWrite(ADCchipSelect, LOW);
   SPI.transfer(CONFIG_ADDR << 1);        // Must shift address 1 bit left for write bit
@@ -95,173 +126,173 @@ void setup() {
   digitalWrite(ADCchipSelect, HIGH);
 
   delay(100);
-  
+
   // Print data header
   Serial.print("channel"); Serial.print(',');
   Serial.print("timeMs");  Serial.print(',');
   Serial.println("peak");
-  
+
   delay(100);
-  
+
   // Configure interrupts
   attachInterrupt(IRQ_CH1, eventISR_CH1, RISING);
   attachInterrupt(IRQ_CH2, eventISR_CH2, RISING);
   attachInterrupt(IRQ_CH3, eventISR_CH3, RISING);
   attachInterrupt(IRQ_CH4, eventISR_CH4, RISING);
-  
+
   Serial.println("INTERRUPTS ARE CONFIGURED");
-  
+
   delay(100);
 }
 
 void loop() {
-  
-  
-  Serial.print("CH. 1 FLAG:"); Serial.print(newEventCH1); Serial.print(" ");
-  Serial.print("CH. 2 FLAG:"); Serial.print(newEventCH2); Serial.print(" ");
-  Serial.print("CH. 3 FLAG:"); Serial.print(newEventCH3); Serial.print(" ");
-  Serial.print("CH. 4 FLAG:"); Serial.print(newEventCH4); Serial.println(" ");
-  
-  if(newEventCH1) {
-    
+
+
+  //Serial.print("CH. 1 FLAG:"); Serial.print(newEventCH1); Serial.print(" ");
+  //Serial.print("CH. 2 FLAG:"); Serial.print(newEventCH2); Serial.print(" ");
+  //Serial.print("CH. 3 FLAG:"); Serial.print(newEventCH3); Serial.print(" ");
+  //Serial.print("CH. 4 FLAG:"); Serial.print(newEventCH4); Serial.println(" ");
+
+  if (newEventCH1) {
+
     timeMs = millis();  // Get timestamp in milliseconds
-    
+
     // Issue A/D command to read MCA channel 1 peak value
     digitalWrite(ADCchipSelect, LOW);
     SPI.transfer(MANUAL_READ_ADDR << 1);
     SPI.transfer(READ_CH1);
     digitalWrite(ADCchipSelect, HIGH);
-    
+
     // Wait one additional frame for data to be ready
     digitalWrite(ADCchipSelect, LOW);
     SPI.transfer(0);
     SPI.transfer(0);
     digitalWrite(ADCchipSelect, HIGH);
-  
+
     // Read out peak value for MCA channel 1
     digitalWrite(ADCchipSelect, LOW);
-    peakCH1 = SPI.transfer(0) & 0x0F; 
+    peakCH1 = SPI.transfer(0) & 0x0F;
     peakCH1 = peakCH1 << 8;
     peakCH1 += SPI.transfer(0);
     digitalWrite(ADCchipSelect, HIGH);
-    
+
     // Reset peak detector
     digitalWrite(RESET_CH1, HIGH);
     delayMicroseconds(5);
     digitalWrite(RESET_CH1, LOW);
-    
+
     Serial.print("1");    Serial.print(',');
     Serial.print(timeMs); Serial.print(',');
     Serial.println(peakCH1);
-    
+
     peakCH1 = 0;
     newEventCH1 = false;
   }
-  
-  if(newEventCH2) {
-    
+
+  if (newEventCH2) {
+
     timeMs = millis();  // Get timestamp in milliseconds
-    
+
     // Issue A/D command to read MCA channel 1 peak value
     digitalWrite(ADCchipSelect, LOW);
     SPI.transfer(MANUAL_READ_ADDR << 1);
     SPI.transfer(READ_CH2);
     digitalWrite(ADCchipSelect, HIGH);
-    
+
     // Wait one additional frame for data to be ready
     digitalWrite(ADCchipSelect, LOW);
     SPI.transfer(0);
     SPI.transfer(0);
     digitalWrite(ADCchipSelect, HIGH);
-  
+
     // Read out peak value for MCA channel 1
     digitalWrite(ADCchipSelect, LOW);
-    peakCH2 = SPI.transfer(0) & 0x0F; 
+    peakCH2 = SPI.transfer(0) & 0x0F;
     peakCH2 = peakCH2 << 8;
     peakCH2 += SPI.transfer(0);
     digitalWrite(ADCchipSelect, HIGH);
-    
+
     // Reset peak detector
     digitalWrite(RESET_CH2, HIGH);
     delayMicroseconds(5);
     digitalWrite(RESET_CH2, LOW);
-    
+
     Serial.print("2");    Serial.print(',');
     Serial.print(timeMs); Serial.print(',');
     Serial.println(peakCH2);
-    
+
     peakCH2 = 0;
     newEventCH2 = false;
   }
-  
-  if(newEventCH3) {
-    
+
+  if (newEventCH3) {
+
     timeMs = millis();  // Get timestamp in milliseconds
-    
+
     // Issue A/D command to read MCA channel 1 peak value
     digitalWrite(ADCchipSelect, LOW);
     SPI.transfer(MANUAL_READ_ADDR << 1);
     SPI.transfer(READ_CH3);
     digitalWrite(ADCchipSelect, HIGH);
-    
+
     // Wait one additional frame for data to be ready
     digitalWrite(ADCchipSelect, LOW);
     SPI.transfer(0);
     SPI.transfer(0);
     digitalWrite(ADCchipSelect, HIGH);
-  
+
     // Read out peak value for MCA channel 1
     digitalWrite(ADCchipSelect, LOW);
-    peakCH3 = SPI.transfer(0) & 0x0F; 
+    peakCH3 = SPI.transfer(0) & 0x0F;
     peakCH3 = peakCH3 << 8;
     peakCH3 += SPI.transfer(0);
     digitalWrite(ADCchipSelect, HIGH);
-    
+
     // Reset peak detector
     digitalWrite(RESET_CH3, HIGH);
     delayMicroseconds(5);
     digitalWrite(RESET_CH3, LOW);
-    
+
     Serial.print("3");    Serial.print(',');
     Serial.print(timeMs); Serial.print(',');
     Serial.println(peakCH3);
-    
+
     peakCH3 = 0;
     newEventCH3 = false;
   }
-  
-  if(newEventCH4) {
-    
+
+  if (newEventCH4) {
+
     timeMs = millis();  // Get timestamp in milliseconds
-    
+
     // Issue A/D command to read MCA channel 1 peak value
     digitalWrite(ADCchipSelect, LOW);
     SPI.transfer(MANUAL_READ_ADDR << 1);
     SPI.transfer(READ_CH4);
     digitalWrite(ADCchipSelect, HIGH);
-    
+
     // Wait one additional frame for data to be ready
     digitalWrite(ADCchipSelect, LOW);
     SPI.transfer(0);
     SPI.transfer(0);
     digitalWrite(ADCchipSelect, HIGH);
-  
+
     // Read out peak value for MCA channel 1
     digitalWrite(ADCchipSelect, LOW);
-    peakCH4 = SPI.transfer(0) & 0x0F; 
+    peakCH4 = SPI.transfer(0) & 0x0F;
     peakCH4 = peakCH4 << 8;
     peakCH4 += SPI.transfer(0);
     digitalWrite(ADCchipSelect, HIGH);
-    
+
     // Reset peak detector
     digitalWrite(RESET_CH4, HIGH);
     delayMicroseconds(5);
     digitalWrite(RESET_CH4, LOW);
-    
+
     Serial.print("4");    Serial.print(',');
     Serial.print(timeMs); Serial.print(',');
     Serial.println(peakCH4);
-    
+
     peakCH4 = 0;
     newEventCH4 = false;
   }
