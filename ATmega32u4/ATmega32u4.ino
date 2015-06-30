86d30ceb908ded6426bfd3c6921f20e4efd5cdbb
alindell <linde641@umn.edu>
2015-06-30 15:35:40 -0500

    added ATmega32u4 code draft

---
+++ ATmega32u4/ATmega32u4.ino
@@ -0,0 +1,42 @@
+#define half_full_int // from FIFO
+#define read_enable // to FIFO
+#define output_enable // to tri state buffer
+#define bale_int // from Tomcat
+#define IOR //from Tomcat
+#define address_valid // from decoder/Tomcat
+#define IRQ_OUT // to Tomcat
+
+volatile bool bale_flag = false;
+
+void half_full_handler(){
+  digitalWrite(IRQ_OUT, HIGH); 
+}
+
+void bale_handler(){
+  bale_flag = true;
+}
+
+void setup() {
+  
+  pinMode(IRQ_OUT, OUTPUT);
+  pinMode(output_enable, OUTPUT);
+  pinMode(read_enable, OUTPUT);
+  
+  attachInterrupt(half_full_int, half_full_handler, RISING); 
+  attachInterrupt(bale_int, bale_handler, RISING); 
+
+}
+
+void loop() {
+  
+  
+  if (bale_flag){
+    if (address_valid && IOR){
+      digitalWrite(read_enable, HIGH);
+      digitalWrite(output_enable, HIGH);
+    }
+    bale_flag = false;
+  }
+
+
+}
