6dacc07138bdcca0be2d9e3810cdbcf6f061ea14
alindell <linde641@umn.edu>
2015-07-02 15:40:38 -0500

    improved ATmega32u4 code

--- ATmega32u4/ATmega32u4.ino
+++ ATmega32u4/ATmega32u4.ino
@@ -1,28 +1,42 @@
-#define half_full_int // from FIFO
-#define read_enable // to FIFO
-#define output_enable // to tri state buffer
-#define bale_int // from Tomcat
-#define IOR //from Tomcat
-#define address_valid // from decoder/Tomcat
-#define IRQ_OUT // to Tomcat
+#define half_full_int 0  // this is the interrupt number, comes from FIFO
+//#define read_enable  // to FIFO
+//#define output_enable // to tri state buffer
+#define bale_int 1 // this is the interrupt number, comes from Tomcat
+//#define IOR // PD2, from Tomcat
+//#define address_valid // PD3, from decoder/Tomcat
+//#define IRQ_OUT // to Tomcat
 
-volatile bool bale_flag = false;
+// 2 interrupts(0 and 1 which are PD0 and PD1), 2 regular inputs, and 3 outputs (PD4, PD6, PD7)
+ 
+volatile bool flush_fifo_flag = false;
 volatile bool half_full_flag = false;
+volatile bool address_valid_flag = false;
+volatile bool IOR_flag = false;
+
 
 void half_full_handler(){
-  digitalWrite(IRQ_OUT, HIGH); 
+  
+  PORTD = PORTD | B10000000; //sets PD7 (IRQ_OUT)
   half_full_flag = true;
 }
 
 void bale_handler(){
-  bale_flag = true;
+  
+  address_valid_flag = PIND & B00000100;
+  IOR_flag = PIND & B00001000;
+  
+  if (address_valid_flag && IOR_flag){ // wrong, these are constants, needs to check the pins they represent
+    flush_fifo_flag = true;
+    address_valid_flag = false;
+    IOR_flag = false;
+  }
 }
 
 void setup() {
   
-  pinMode(IRQ_OUT, OUTPUT);
-  pinMode(output_enable, OUTPUT);
-  pinMode(read_enable, OUTPUT);
+  DDRD = B11010000;
+  // PD7, PD6, and PD4 will be outputs IRQ_OUT, output_enable, and read_enable
+  // PD7 = DigitalPin 6, PD6 = DigitalPin 12, PD4 = DigitalPin4
   
   attachInterrupt(half_full_int, half_full_handler, RISING); 
   attachInterrupt(bale_int, bale_handler, RISING); 
@@ -31,20 +45,13 @@ void setup() {
 
 void loop() {
   
-  
-  if (bale_flag){
-    if (address_valid && IOR){
-      digitalWrite(read_enable, HIGH);
-      digitalWrite(output_enable, HIGH);
-      digitalWrite(address_valid, LOW); // not sure about this
-      digitalWrite(IOR, LOW); //not sure about this
-      digitalWrite(read_enable, LOW); //not sure how long these need to be set for
-      digitalWrite(output_enable, LOW);
-    }
-    bale_flag = false;
+  if (flush_fifo_flag){
+    PORTD = PORTD | B01010000  // set read_enable and output_enable
+    PORTD = PORTD & ~B11010000 // turn off read_enable, output_enable, and IRQ_OUT
+    flush_fifo_flag = false;
   }
   
-  if (half_full_flag){
-    digitalWrite(IRQ_OUT, LOW);
-    half_full_flag = false;
+  if (half_full_flag){ // just turns off everything that half_full_handler turns on
+    PORTD = PIND | B0000000; //turn off PD7/IRQ_OUT
+    half_full_flag = false; 
   }
