8cc8af67a4349d9d08af5d6e4c76d92719ecb0de
Luke Granlund <e3dflyer95@gmail.com>
2015-07-03 11:38:53 -0500

    Files for GPS

---
+++ gps_novatel.h
@@ -0,0 +1,19 @@
+#ifndef GPS_NOVATEL_H_
+#define GPS_NOVATEL_H_
+
+#include <stdint.h>
+#include "globaldefs.h"
+
+#define GPS_RST 61
+#define GPS_POS_VAL 44
+
+void init_GPS(struct gps *gpsData_ptr);
+int read_GPS(struct gps *gpsData_ptr);
+//void init_GPS();
+//int read_GPS();
+void endian_swap(uint8_t *buf, int index, int count);
+unsigned int CRC32Value(int i);
+unsigned int CalculateBlockCRC32(unsigned int ulCount, unsigned char *ucBuffer);
+
+#endif
+
