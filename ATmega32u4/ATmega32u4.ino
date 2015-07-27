10ecad7b84243d1d36d698573336c6c97f094b96
alindell <linde641@umn.edu>
2015-07-27 16:00:56 -0500

    update 32u4

--- ATmega32u4/ATmega32u4.ino
+++ ATmega32u4/ATmega32u4.ino
@@ -8,15 +8,14 @@
 #define IOCHRDY B00010000 // to tomcat, PF4
 #define IOCHRDY_READ_OUTPUT_ENABLE B00110001 
 
-volatile bool bale_int_flag = false;
-volatile bool half_full_flag = false;
-//volatile bool address_valid_flag = false;
-//volatile bool IOR_flag = false;
+volatile byte HF_count = 0;
+volatile byte bale_count = 0;
 
 void half_full_handler(){
   
   PORTF |= IRQ_OUT;
   PORTF &= ~IRQ_OUT;
+  HF_count++;  
 }
 
 void bale_handler(){
@@ -25,20 +24,26 @@ void bale_handler(){
     PORTF &= ~IOCHRDY_READ_OUTPUT_ENABLE;
     PORTF |= IOCHRDY_READ_OUTPUT_ENABLE;    
   }
+ // bale_count++;
   
 }
 
 void setup() {
   DDRF = IRQ_OUT | OUTPUT_ENABLE | FIFO_READ_ENABLE | IOCHRDY;  //set pins to output mode
-  PORTF = PORTF | FIFO_READ_ENABLE | IOCHRDY | OUTPUT_ENABLE; // setting read_enable high initially; this is a requirement so that the FIFO can be reset correctly by atmega2560
+  PORTF = PORTF | FIFO_READ_ENABLE | IOCHRDY | OUTPUT_ENABLE & ~IRQ_OUT; // setting read_enable high initially; this is a requirement so that the FIFO can be reset correctly by atmega2560
   // also setting IOCHRDY high by default, it is active low
   // also setting OUTPUT_ENABLE high (off) initially so tomcat can correctly start up
   attachInterrupt(HALF_FULL_INT, half_full_handler, FALLING); // this line is active low from FIFO 
   attachInterrupt(BALE_INT, bale_handler, RISING);  
-
+  Serial.begin(115200); Serial.flush();
+  delay(60000);
+  Serial.println("Starting");
 }
 
 void loop() {
 // just waiting for interrupts
+  Serial.print("HF: "); Serial.println(HF_count);
+  Serial.print("Bale: "); Serial.println(bale_count);
+  delay(500);
 
 }
